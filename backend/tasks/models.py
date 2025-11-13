dde5d03b307d03ce86e29e7774a8049a6f83c12e
psatomas <psatomas@gmail.com>
2025-11-13 18:14:28 -0300

    feat: add Task model

--- backend/tasks/models.py
+++ backend/tasks/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Task(models.Model):
+    name = models.CharField(max_length=255)
+    status = models.CharField(max_length=50, default="pending")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
