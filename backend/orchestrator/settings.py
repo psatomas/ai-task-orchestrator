ce541cae883f2f1dbf3eead58daf540973f95b92
psatomas <psatomas@gmail.com>
2025-11-13 15:31:23 -0300

    feat: add Celery integration with Redis

--- backend/orchestrator/settings.py
+++ backend/orchestrator/settings.py
@@ -120,3 +120,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CELERY_BROKER_URL = "redis://redis:6379/0"
+CELERY_RESULT_BACKEND = "redis://redis:6379/0"
