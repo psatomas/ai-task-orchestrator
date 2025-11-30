f0d62c590a5c55cd3a996f3581e6cef8f6179a48
psatomas <psatomas@gmail.com>
2025-11-30 11:31:18 -0300

    feat(auth): add custom User model and reset migrations

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    """Custom user model extending AbstractUser."""
+
+    def __str__(self):
+        return self.username
